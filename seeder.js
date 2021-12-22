06db4c63afe62b6e3683503d3d355ece54e6b9a3
MohammedYoussefSoliman <m.youssef.soliman@gmail.com>
2021-12-22 11:03:21 +0200

    finished seeder

---
+++ seeder.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const mongoose = require("mongoose");
+const colors = require("colors");
+const dotenv = require("dotenv");
+dotenv.config({
+  path: "./.env",
+});
+// database
+const connectDatabase = require("./utils/database");
+// models
+const Bootcamps = require("./models/Bootcamps");
+// seeder data
+const bootampsData = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
+);
+connectDatabase();
+
+async function importData() {
+  try {
+    await Bootcamps.create(bootampsData);
+    console.log("seeder imported bootCamps".green.inverse);
+    process.exit();
+  } catch (error) {
+    console.log("seeder failed to import bootcamps".red.inverse);
+  }
+}
+
+async function destroyData() {
+  try {
+    await Bootcamps.deleteMany();
+    console.log("seeder destroyed bootCamps".red.inverse);
+    process.exit();
+  } catch (error) {
+    console.log("seeder failed to destroy bootcamps".red.inverse);
+  }
+}
+
+if (process.argv[2] === "-i") importData();
+if (process.argv[2] === "-d") destroyData();
